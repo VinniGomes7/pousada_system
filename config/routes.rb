2e20e712af5567b18edb505b921dc2ad19079458
root <root@LENOVO-IDEAPAD3R7U>
2024-10-10 11:21:58 -0300

    Atualizações na view e controller de Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  # Remover a linha do Devise
+  get 'home/index'
   # devise_for :users
   
-  # Manter as rotas existentes
   resources :users
   resources :quartos
   resources :reservas
   resources :funcionarios
   resources :hospedes
 
-  # Define a rota root para a página inicial
-  root "hospedes#index"
+  root "home#index"
 end
